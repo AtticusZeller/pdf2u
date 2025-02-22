4a527d8c8db15222a755601b4e8b6ead98db1bed
Atticuszz <1831768457@qq.com>
2025-02-22 22:28:19 +0800

    fix: cache path

--- src/pdf2u/const.py
+++ src/pdf2u/const.py
@@ -1,7 +1,8 @@
+import tempfile
 from enum import StrEnum
 from pathlib import Path
 
-CACHE_FOLDER = Path.home() / ".cache" / "pdf2u"
+CACHE_FOLDER = Path(tempfile.gettempdir()) / "pdf2u_cache"
 
 
 def get_cache_file_path(filename: str) -> Path:
